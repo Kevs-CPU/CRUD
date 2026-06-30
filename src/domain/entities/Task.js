bacd814383f35c029859c524d4f8274311451154
John Kevin <jameo@example.com>
2026-06-30 15:52:08 +0800

    Refactor and format todo lsit code

--- src/domain/entities/Task.js
+++ src/domain/entities/Task.js
@@ -1,7 +1,6 @@
 export class Task {
   constructor({ id, title, list = "default", done = false }) {
     
-    
     this.id = id;
     this.title = title;
                             //this.list = list; 

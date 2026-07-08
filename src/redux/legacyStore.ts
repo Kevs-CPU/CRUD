9b829b3b557b4468cf0a632111c56e39ea93f1de
John Kevin <jameo@example.com>
2026-07-08 09:05:05 +0800

    Refactor

--- src/redux/legacyStore.ts
+++ src/redux/legacyStore.ts
@@ -4,7 +4,7 @@ import { taskReducer } from "./task/task.reducers";
 
 const rootReducer = combineReducers({ tasks: taskReducer });
 
-export const legacyStore = createStore(rootReducer, applyMiddleware(thunk));  
+export const legacyStore = createStore(rootReducer, applyMiddleware(thunk)); 
 
 
 
--- src/redux/task/task.actions.ts
+++ src/redux/task/task.actions.ts
@@ -1,4 +1,11 @@
 import { ADD_TASK, REMOVE_TASK, UPDATE_TASK, GET_ALL_TASKS, GET_TASK } from './task.types';
+import {
+  addTaskUseCase,
+  removeTaskUseCase,
+  updateTaskUseCase,
+  getAllTasksUseCase,
+  getTaskUseCase,
+} from '../../app/taskUseCaseProvider';
 
 interface Task {
   id: string;
@@ -29,3 +36,28 @@ export const getTask = (task: Task) => ({
   type: GET_TASK,
   payload: task,
 });
+
+export const fetchAllTasksThunk = () => async (dispatch: any) => {
+  const tasks = await getAllTasksUseCase.execute();
+  dispatch(getAllTasks(tasks));
+};
+
+export const fetchTaskThunk = (id: string) => async (dispatch: any) => {
+  const task = await getTaskUseCase.execute(id);
+  dispatch(getTask(task));
+};
+
+export const addTaskThunk = (title: string) => async (dispatch: any) => {
+  const task = await addTaskUseCase.execute({ title });
+  dispatch(addTask(task));
+};
+
+export const removeTaskThunk = (id: string) => async (dispatch: any) => {
+  await removeTaskUseCase.execute(id);
+  dispatch(removeTask(id));
+};
+
+export const updateTaskThunk = (id: string, changes: Partial<Task>) => async (dispatch: any) => {
+  const updated = await updateTaskUseCase.execute(id, changes);
+  dispatch(updateTask(id, { title: updated.title }));
+};
